8c88db5bed19a0a9bd516a820c97193c19bf6f9c
KawalerZero <adamkowalczyk12@outlook.com>
2017-05-14 16:52:49 +0200

    Add possibility to create a file on Google Drive

--- app/src/main/java/wolvesfromuz/androidbackupapp/MainActivity.java
+++ app/src/main/java/wolvesfromuz/androidbackupapp/MainActivity.java
@@ -1,5 +1,6 @@
 package wolvesfromuz.androidbackupapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //todo Need to call some other Activity which will provide the choice of createBackUp or UpdateBackUp
+        Intent intent = new Intent(getBaseContext(), CreateFileActivity.class);
+        startActivity(intent);
+
     }
 }
